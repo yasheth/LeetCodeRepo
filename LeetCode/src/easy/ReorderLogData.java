3f654d8e2cc184599f788c777f88eee34e26e275
Yash Sheth <yasheth77@gmail.com>
2020-06-26 08:23:12 -0400

    Added ReorderLogData

---
+++ LeetCode/src/easy/ReorderLogData.java
@@ -0,0 +1,43 @@
+package easy;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class ReorderLogData {
+
+	public static String[] reorderLogFiles(String[] logs) {
+
+		Comparator<String> myComp = new Comparator<String>() {
+			@Override
+			public int compare(String s1, String s2) {
+				int s1SpaceIndex = s1.indexOf(' ');
+				int s2SpaceIndex = s2.indexOf(' ');
+				char s1FirstCharacter = s1.charAt(s1SpaceIndex + 1);
+				char s2FirstCharacter = s2.charAt(s2SpaceIndex + 1);
+
+				if (s1FirstCharacter <= '9') {
+					if (s2FirstCharacter <= '9')
+						return 0;
+					else
+						return 1;
+				}
+				if (s2FirstCharacter <= '9')
+					return -1;
+
+				int preCompute = s1.substring(s1SpaceIndex + 1).compareTo(s2.substring(s2SpaceIndex + 1));
+				if (preCompute == 0)
+					return s1.substring(0, s1SpaceIndex).compareTo(s2.substring(0, s2SpaceIndex));
+				return preCompute;
+			}
+		};
+
+		Arrays.sort(logs, myComp);
+		return logs;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(reorderLogFiles(new String[] { "dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero" }));
+
+	}
+
+}
